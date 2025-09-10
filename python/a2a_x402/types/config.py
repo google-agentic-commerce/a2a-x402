e8dc47d23f87b70a3a20a9376716a7a9725b1e1a
Jordan Ellis <jordanellis@google.com>
2025-09-10 19:18:50 +0000

    add license headers for cross verifications

--- python/a2a_x402/types/config.py
+++ python/a2a_x402/types/config.py
@@ -1,3 +1,16 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Configuration types for a2a_x402."""
 
 from typing import Optional, Union
@@ -28,4 +41,3 @@ class X402ServerConfig(BaseModel):
     max_timeout_seconds: int = 600
     resource: Optional[str] = None
     asset_address: Optional[str] = None
-
